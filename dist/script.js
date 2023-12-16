f3521d6078cabf87b087493575fd344b42d469a0
Jakub Gryczka <gryczka.jakub@gmail.com>
2023-12-16 17:45:39 +0100

    Fix date calculation and error handling

--- dist/script.js
+++ dist/script.js
@@ -4,15 +4,22 @@ const btn = document.querySelector(".btn");
 const inputs = document.querySelectorAll(".input");
 const errorElement = document.querySelectorAll(".errorMessage");
 const formItem = document.querySelectorAll(".form__item");
+const years = document.querySelector(".years");
+const months = document.querySelector(".months");
+const days = document.querySelector(".days");
 
 const [firstItem, secondItem, thirdItem] = formItem;
 const [day, month, year] = inputs;
 const [dayError, monthError, yearError] = errorElement;
 
 const date = new Date();
-const currentDay = date.getDate();
+const currentDay = date.getDay();
 const currentMonth = date.getMonth() + 1;
 const currentYear = date.getFullYear();
+
+function checkInput(type) {
+  this.errorMessage.push(`Must be a valid ${type}}`);
+}
 
 form.addEventListener("submit", (e) => {
   let validate = {
@@ -35,17 +42,21 @@ form.addEventListener("submit", (e) => {
       errorMessage: [],
     },
   };
+  e.preventDefault();
+
+  let hasErrors = false;
+
   if (day.value > 31 || day.value < 1) {
-    e.preventDefault();
     validate.day.errorMessage.push("Must be a valid day");
+    hasErrors = true;
   }
   if (month.value > 12 || month.value < 1) {
-    e.preventDefault();
     validate.month.errorMessage.push("Must be a valid month");
+    hasErrors = true;
   }
   if (year.value > currentYear) {
-    e.preventDefault();
     validate.year.errorMessage.push("Must be in the past");
+    hasErrors = true;
   }
 
   for (const { item, input, errorElement, errorMessage } of Object.values(
@@ -53,12 +64,27 @@ form.addEventListener("submit", (e) => {
   )) {
     if (input.value === "" || input.value === null) {
       errorMessage.push("This field is required");
+      hasErrors = true;
     }
     if (errorMessage.length > 0) {
       item.classList.add("error");
       errorElement.classList.remove("hidden");
       errorElement.textContent = errorMessage.slice(-1);
-      e.preventDefault();
+    } else if (errorMessage.length === 0) {
+      item.classList.remove("error");
+      errorElement.classList.add("hidden");
     }
+  }
+
+  if (!hasErrors) {
+    const dateInput = new Date(year.value, month.value - 1, day.value);
+    const calculatedDates = {
+      day: currentDay - dateInput.getDay(),
+      month: currentMonth - dateInput.getMonth() - 1,
+      year: currentYear - dateInput.getFullYear(),
+    };
+    years.textContent = calculatedDates.year;
+    months.textContent = calculatedDates.month;
+    days.textContent = calculatedDates.day;
   }
 });
